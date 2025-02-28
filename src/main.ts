e46daa51079b63d064cdf47f530a46275748ba3d
alessio <alessiomazzella.dev@gmail.com>
2025-02-28 09:05:58 +0100

    Set up basic Express server with environment configuration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
-const main = () => {
-  console.log('Hello, world!');
-};
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
-main();
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+// Test the server
+app.use('/', (req: Request, res: Response) => {
+  res.send('Server OK');
+});
+
+app.listen(process.env.PORT || 3005, (err) => {
+  if (err) {
+    console.log(`Server error at port ${process.env.PORT || 3005}`);
+    return;
+  }
+
+  console.log(`Server started at http://localhost:${process.env.PORT || 3000}`);
+});
